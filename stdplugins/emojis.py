6a09eb12b49d4f91542892d69604098312f64c0a
Shivam Choudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-07-06 07:54:34 +0530

    Update emojis.py

--- stdplugins/emojis.py
+++ stdplugins/emojis.py
@@ -1,23 +1,38 @@
 """Emoji
 Available Commands:
-.emoji shrug
-.emoji apple
-.emoji :/
-.emoji -_-"""
+.shrug
+.apple
+.:/
+.-_-"""
 from telethon import events
 import asyncio
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("emoji (.*)"))
+@borg.on(admin_cmd("(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.3
-    animation_ttl = range(0, 16)
+    animation_interval = 0.5
+    animation_ttl = range(0, 35)
     input_str = event.pattern_match.group(1)
     if input_str == "shrug":
-        await event.edit("¯\_(ツ)_/¯")
+        await event.edit(input_str)
+        animation_chars = [
+            "¯\_(ツ)_/¯",
+            "-____(ツ)____-",
+            "¯\_(ツ)_/¯",
+            "-____(ツ)____-",
+            "¯\_(ツ)_/¯",
+            "-____(ツ)____-",
+            "¯\_(ツ)_/¯",
+            "-____(ツ)____-",
+            "¯\_(ツ)_/¯",
+            "-____(ツ)____-"
+        ]
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 10])
     elif input_str == "apple":
         await event.edit("\uF8FF")
     elif input_str == ":/":
@@ -33,8 +48,18 @@ async def _(event):
         await event.edit(input_str)
         animation_chars = [
             "-__-",
+            "-_-",
+            "-__-",
+            "-___-",
+            "-____-",
+            "-_____-",
+            "-______-",
+            "-_______-",
+            "-________-",
+            "-_________-",
+            "-__________-",
             "-_-"
         ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 2])
+            await event.edit(animation_chars[i % 11])
