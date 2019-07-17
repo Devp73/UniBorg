bb459978c187f536431ca35987bb69a15f61a957
Shivam Choudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-07-17 10:51:42 +0530

    Update eval.py

--- stdplugins/eval.py
+++ stdplugins/eval.py
@@ -6,6 +6,7 @@ Syntax: .eval PythonCode"""
 from telethon import events, sync, errors, functions, types
 import inspect
 import io
+import asyncio
 from uniborg.util import admin_cmd
 
 
@@ -19,6 +20,7 @@ async def _(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     evaluation = eval(cmd)
+    await event.delete()
     # https://t.me/telethonofftopic/43873
     # https://t.me/TheUseLessGroup/40472
     try:
@@ -27,18 +29,3 @@ async def _(event):
     except (Exception) as e:
         evaluation = str(e)
     # https://t.me/telethonofftopic/43873
-    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-    if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-            await event.delete()
-    else:
-        await event.edit(final_output)
